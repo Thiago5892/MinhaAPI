a00a67549733ddaa6441695ccfaef4fee1db9c21
Thiago Borba <thiagoborba8@gmail.com>
2023-01-15 19:37:43 -0300

    Atualizando

--- MinhaAPI/Model/UsuarioModel.cs
+++ MinhaAPI/Model/UsuarioModel.cs
@@ -3,9 +3,9 @@
     public class UsuarioModel
     {
         public int Id { get; set; }
-        public string? Nome { get; set; }
+        public string Nome { get; set; }
 
-        public string? Email { get; set; }       
+        public string Email { get; set; }       
 
     }
 
